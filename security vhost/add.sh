669843e354890cf9cbc2ea1d24df86e37c9e9500
Hcamael <jrh_ly@163.com>
2016-04-28 13:52:18 +0800

    modify

--- security vhost/add.sh
+++ security vhost/add.sh
@@ -10,7 +10,6 @@ chown www:www /tmp/php-cgi-$1.sock
 cp /usr/local/php/etc/php-fpm.conf /usr/local/php/etc/php-fpm-$1.conf
 sed -i "s/www/$1/g" /usr/local/php/etc/php-fpm-$1.conf
 sed -i "s/php-fpm/php-fpm-$1/g" /usr/local/php/etc/php-fpm-$1.conf
-sed -i "s/php-cgi/php-cgi-$1/g" /usr/local/php/etc/php-fpm-$1.conf
 echo $1 | lnmp vhost add
 sed -i "s/php-cgi/php-cgi-$1/g" /usr/local/php/etc/php-fpm-$1.conf /usr/local/nginx/conf/vhost/$1.conf
 /etc/init.d/php-fpm start $1
