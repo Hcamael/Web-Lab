c2438cbb5a5ae7b6baf108cf6c6e4e48f67503a7
Hcamael <jrh_ly@163.com>
2016-10-27 13:10:16 +0800

    nulti-version php

--- security vhost/diff_php_vhost_add.sh
+++ security vhost/diff_php_vhost_add.sh
@@ -3,17 +3,21 @@ if [ ! -n "$1" ];then
         echo "Please enter vhost name";
         exit 1
 fi
+if [ ! -n "$2" ];then
+        echo "Please enter php version";
+        exit 1
+fi
 groupadd $1
 useradd $1 -M -s /sbin/nologin -g $1
 
-cat >/usr/local/php/etc/php-fpm-$1.conf<<EOF
+cat >/root/.phpenv/versions/$2/etc/php-fpm.conf<<EOF
 [global]
-pid = /usr/local/php/var/run/php-fpm-$1.pid
-error_log = /usr/local/php/var/log/php-fpm-$1.log
+pid = /root/.phpenv/versions/$2/var/run/php-fpm.pid
+error_log = /root/.phpenv/versions/$2/var/log/php-fpm.log
 log_level = notice
 
 [$1]
-listen = /tmp/php-cgi-$1.sock
+listen = /tmp/php-cgi-$2.sock
 #listen = 127.0.0.1:9000
 listen.backlog = -1
 listen.allowed_clients = 127.0.0.1
@@ -30,11 +34,12 @@ pm.min_spare_servers = 1
 pm.max_spare_servers = 6
 request_terminate_timeout = 100
 request_slowlog_timeout = 0
-slowlog = var/log/slow-$1.log
+slowlog = var/log/slow-$2.log
 EOF
 
 echo $1 | lnmp vhost add
 chown $1:$1 -R /home/wwwroot/$1
-sed -i "s/php-cgi/php-cgi-$1/g" /usr/local/nginx/conf/vhost/$1.conf
-/etc/init.d/php-fpm restart $1
+sed -i "s/php-cgi/php-cgi-$2/g" /usr/local/nginx/conf/vhost/$1.conf
+
+/root/php-fpm.sh restart $2
 /etc/init.d/nginx restart
